03e27756e9ada42afb3cfd321b04fcf56557bc38
HASAN NAJIB <mashasanr@gmail.com>
2025-06-16 22:04:55 +0700

    adding: route parameters to pages

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute();
+// console.log(route);
+</script>
+
+
+<template>
+    <h1>Blog Page</h1>
+    <p>this is {{ route.params.slug }} the blog page.</p>
+</template>
