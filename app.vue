c9cf42944850c59049210107bfd8f80e7ac5a23f
HASAN NAJIB <mashasanr@gmail.com>
2025-06-17 23:05:50 +0700

    determine the page layout

--- app.vue
+++ app.vue
@@ -1,15 +1,6 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-        <li><NuxtLink to="/project">Project</NuxtLink></li>
-      </ul>
-    </nav>
-  </div>
-  <div>
+  <NuxtLayout name="default">
+    <h1>hhahaha</h1>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+                <li><NuxtLink to="/project">Project</NuxtLink></li>
+            </ul>
+        </nav>
+  </div>
+  <slot/>
+</template>
