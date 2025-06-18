24d632ada0194c6fd2902e3271adf5a893c6eed1
HASAN NAJIB <mashasanr@gmail.com>
2025-06-18 16:05:24 +0700

    Styling Layout With Tailwindcss

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li><NuxtLink to="/">Home</NuxtLink></li>
-                <li><NuxtLink to="/about">About</NuxtLink></li>
-                <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-                <li><NuxtLink to="/project">Project</NuxtLink></li>
-            </ul>
-        </nav>
+    <div class="container mx-auto">
+        <header class="flex justify-between items-center">
+            <div>
+                <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-300">Porto</NuxtLink>
+            </div>
+            <nav class="font-mono">
+                <ul class="flex space-x-4">
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+                    <li><NuxtLink to="/project">Project</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="p-4">
+            <slot/>
+        </main>
     </div>
-        <slot/>
 </template>
