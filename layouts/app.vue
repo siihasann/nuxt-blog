c54009532272f6f9199e8b2155a802cdcab1104c
HASAN NAJIB <mashasanr@gmail.com>
2025-06-19 22:13:36 +0700

    adding seo page configuration

--- layouts/app.vue
+++ layouts/app.vue
@@ -13,6 +13,17 @@ useHead({
         },
     ],
 });
+
+useSeoMeta({
+    title: 'My Amazing Site',
+    ogTitle: 'My Amazing Site',
+    description: 'This is my amazing site, let me tell you all about it.',
+    ogDescription: 'This is my amazing site, let me tell you all about it.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+    twitterTitle: 'My Amazing Site',
+    twitterDescription: 'This is my amazing site, let me tell you all about it.',
+});
 </script>
 
 <template>
