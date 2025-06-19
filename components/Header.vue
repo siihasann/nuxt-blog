d7a0772d1767ac0a8d969f483c68bdfd7ff2833f
HASAN NAJIB <mashasanr@gmail.com>
2025-06-19 21:37:25 +0700

    creating: separate components

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="flex justify-between items-center">
+    <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Porto</NuxtLink>
+    </div>
+        <nav class="font-mono">
+            <ul class="flex space-x-4">
+            <li><NuxtLink to="/" class="p-2 hover:bg-gray-200">Home</NuxtLink></li>
+            <li><NuxtLink to="/about" class="p-2 hover:bg-gray-200">About</NuxtLink></li>
+            <li><NuxtLink to="/blog" class="p-2 hover:bg-gray-200">Blog</NuxtLink></li>
+            <li><NuxtLink to="/project" class="p-2 hover:bg-gray-200">Project</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+</template>
--- layouts/app.vue
+++ layouts/app.vue
@@ -1,18 +1,7 @@
 <template>
     <div class="container mx-auto">
-        <header class="flex justify-between items-center">
-            <div>
-                <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-300">Porto</NuxtLink>
-            </div>
-            <nav class="font-mono">
-                <ul class="flex space-x-4">
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-                    <li><NuxtLink to="/project">Project</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
+        <Header/>
+
         <main class="p-4">
             <slot/>
         </main>
